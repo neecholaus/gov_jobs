36f2b0c6ea2c2fd289408d5a5e3363cbc560248c
Nick Neuman <neecholaus@gmail.com>
2018-06-27 15:32:20 -0700

    Search is functional.

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,29 @@
 let main_search_input = document.getElementById('main-search-input');
 let main_search_submit = document.getElementById('main-search-submit');
 
+var data = [];
 
 main_search_submit.addEventListener('click', function() {
 	let search = main_search_input.value;
 	let url = 'https://jobs.search.gov/jobs/search.json?size=100&query=';
 
 	axios.get(`${url}${search}`).then(function(response) {
-		let data = response.data;
-		console.log(data);
+		let res = response.data;
+
+		data.splice(0, data.length);
+		for(let i = 0; i < res.length; i++) {
+			data.push(res[i]);
+		}
 	}).catch(function(error) {
 		console.log(error);
 	});
 });
+
+let app = new Vue({
+	el: '#results',
+	data: function() {
+		return {
+			results: data
+		}
+	}
+})
